8da452aa3c9032b72747b13409816daf175c2f3d
PHANTOM3114 <m.himochko@gmail.com>
2026-01-08 23:51:54 +0100

    refactor(metahub): parse pipelines to qvector

--- metahub/src/cicd_view_model/include/cicd_view_model.hpp
+++ metahub/src/cicd_view_model/include/cicd_view_model.hpp
@@ -1,27 +1,36 @@
-#pragma once 
+#pragma once
 
-#include <QObject>
-#include <QDebug>
+#include <qcontainerfwd.h>
+#include <qtypes.h>
 #include <QDBusInterface>
 #include <QDBusPendingCallWatcher>
 #include <QDBusPendingReply>
+#include <QDebug>
+#include <QObject>
 
-class CiCDViewModel : public QObject
-{
+struct PipelineData {
+    qint64 m_id;
+    QString m_status;
+    QString m_pipeline_name;
+    QString m_conclusion;
+};
+
+class CiCDViewModel : public QObject {
     Q_OBJECT
 public:
-    explicit CiCDViewModel(QObject *parent = nullptr);
+    explicit CiCDViewModel(QObject* parent = nullptr);
 
 signals:
-    void cicdDataReceived(const QString &data);
-    void cicdErrorReceived(const QString &error);
+    void cicdDataReceived(const QString& data);
+    void cicdErrorReceived(const QString& error);
 
 public slots:
     void fetchCicdData();
 
 private slots:
-    void onDataFetchFinished(QDBusPendingCallWatcher *watcher);
+    void onDataFetchFinished(QDBusPendingCallWatcher* watcher);
 
 private:
-    QDBusInterface *m_dbusInterface;
+    QDBusInterface* m_dbusInterface;
+    QVector<PipelineData> m_pipline_data;
 };
--- metahub/src/cicd_view_model/src/cicd_view_model.cpp
+++ metahub/src/cicd_view_model/src/cicd_view_model.cpp
@@ -1,49 +1,70 @@
 // components/metahub/src/cicd_manager.cpp
 
 #include "cicd_view_model.hpp"
+#include <qcontainerfwd.h>
+#include <qlogging.h>
+#include <qtdbusglobal.h>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 const QString CICD_SERVICE_NAME = "org.metaos.CiCdService";
 const QString CICD_OBJECT_PATH = "/org/metaos/CiCd";
 const QString CICD_INTERFACE_NAME = "org.metaos.CiCd.Interface";
 
-CiCDViewModel::CiCDViewModel(QObject *parent) : QObject(parent)
-{
-    m_dbusInterface = new QDBusInterface(
-        CICD_SERVICE_NAME,
-        CICD_OBJECT_PATH,
-        CICD_INTERFACE_NAME,
-        QDBusConnection::sessionBus(),
-        this
-    );
+CiCDViewModel::CiCDViewModel(QObject* parent) : QObject(parent) {
+    m_dbusInterface = new QDBusInterface(CICD_SERVICE_NAME, CICD_OBJECT_PATH, CICD_INTERFACE_NAME, QDBusConnection::sessionBus(), this);
 
     if (!m_dbusInterface->isValid()) {
         qWarning() << "D-Bus interface is NOT valid:" << CICD_SERVICE_NAME;
     }
 }
 
-void CiCDViewModel::fetchCicdData()
-{
+void CiCDViewModel::fetchCicdData() {
     qInfo() << "QML button clicked, calling D-Bus method FetchData...";
 
     QDBusPendingCall asyncCall = m_dbusInterface->asyncCall("PipelineStatusFetch");
-    QDBusPendingCallWatcher *watcher = new QDBusPendingCallWatcher(asyncCall, this);
-    
+    QDBusPendingCallWatcher* watcher = new QDBusPendingCallWatcher(asyncCall, this);
+
     connect(watcher, &QDBusPendingCallWatcher::finished, this, &CiCDViewModel::onDataFetchFinished);
 }
 
-void CiCDViewModel::onDataFetchFinished(QDBusPendingCallWatcher *watcher)
-{
+void CiCDViewModel::onDataFetchFinished(QDBusPendingCallWatcher* watcher) {
     QDBusPendingReply<QString> reply = *watcher;
 
     if (reply.isValid()) {
-        QString result = reply.value();
-        qInfo() << "D-Bus Reply OK:" << result;
+        QByteArray result = reply.value().toUtf8();
+        qInfo() << "D-Bus Reply OK:";
+
+        m_pipline_data.clear();
+
+        QJsonDocument doc;
+        doc = QJsonDocument::fromJson(result);
+
+        if (!doc.isArray()) {
+            qWarning() << "Unvalid Json";
+            return;
+        } else {
+            QJsonArray json_array = doc.array();
+
+            for (auto pipeline : json_array) {
+                PipelineData pipeline_data;
+                QJsonObject object = pipeline.toObject();
+
+                pipeline_data.m_id = object["id"].toInteger();
+                pipeline_data.m_pipeline_name = object["name"].toString();
+                pipeline_data.m_status = object["status"].toString();
+                pipeline_data.m_conclusion = object["conclusion"].toString();
+
+                m_pipline_data.push_back(pipeline_data);
+            }
+        }
+
         emit cicdDataReceived(result);
-    } 
-    else {
+    } else {
         QString error = reply.error().message();
         qWarning() << "D-Bus Call Failed:" << error;
-        emit cicdErrorReceived(error); 
+        emit cicdErrorReceived(error);
     }
 
     watcher->deleteLater();
