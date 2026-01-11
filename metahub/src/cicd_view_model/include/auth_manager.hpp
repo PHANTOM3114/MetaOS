6bfe9b1a0ff987552f952ecaea2f22ecccfd3d24
PHANTOM3114 <m.himochko@gmail.com>
2026-01-11 13:46:30 +0100

    feat(auth): add AuthManager header  for OAuth2 flow

---
+++ metahub/src/cicd_view_model/include/auth_manager.hpp
@@ -0,0 +1,23 @@
+#include <qobject.h>
+#include <qtmetamacros.h>
+
+class QOAuth2AuthorizationCodeFlow;
+class QOAuthHttpServerReplyHandler;
+
+class AuthManager : public QObject {
+    Q_OBJECT
+
+public:
+    explicit AuthManager(QObject* parent = nullptr);
+
+signals:
+    void tokenReceived(const QString& token);
+    void tokenErrorReceived(const QString& error);
+
+private slots:
+    void onAuthFinished();
+
+private:
+    QOAuth2AuthorizationCodeFlow* m_oauth;
+    QOAuthHttpServerReplyHandler* m_handler;
+};
---
+++ metahub/src/cicd_view_model/src/auth_manager.cpp
@@ -0,0 +1,5 @@
+#include "auth_manager.hpp"
+#include <QDesktopServices>
+#include <QOAuth2AuthorizationCodeFlow>
+#include <QOAuthHttpServerReplyHandler>
+#include <QUrl>
